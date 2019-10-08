2c38cbac4bb2172064f502e73d0b93ffab90c47a
konstantinos lazaros <konlazaros@gmail.com>
2019-10-08 14:59:52 +0300

    third commit

--- main.c
+++ main.c
@@ -69,7 +69,7 @@ void print_score(int*);
 
 void snake_init(snake *snake1)
 {
-    snake1->symbol='*';
+    snake1->symbol='@';
     snake1->size=10;
     snake1->direction=right;
     snake1->prev_direction=down;
@@ -100,19 +100,160 @@ void set_borders()
     for (i=0; i<vertical; ++i)
     {
         gotoxy(0,i);
-        printf("X");
+        printf("|");
         gotoxy(horizontal,i);
-        printf("X");
+        printf("|");
     }
 
     for (i=0; i<horizontal; ++i)
         {
             gotoxy(i,0);
-            printf("X");
+            printf("-");
             gotoxy(i,vertical);
-            printf("X");
+            printf("-");
         }
 }
+
+
+void snake_move(snake *snake1, snake_pos *pos1, food *food1, int *score)
+{
+    move_head(snake1, pos1);
+
+    if(!((snake1 -> head_X == food1 -> X) && (snake1 -> head_Y == food1 -> Y)))
+    {
+        move_tail(snake1, pos1);
+    }
+    else
+    {
+        snake1 -> size++;
+        *score = *score + 10;
+        food1 -> X = rand() % (horizontal - 5);
+        food1 -> Y = rand() % (vertical - 5);
+        food_print(food1);
+    }
+}
+
+
+void move_tail(snake *snake1, snake_pos *pos1)
+{
+    int i;
+
+    //removing the last cell from the tail
+    gotoxy(pos1 -> X[0], pos1 -> Y[0]);
+    printf(" ");
+
+    //update of the new position of the tail
+    for(i = 0; i < snake1 -> size; ++i)
+    {
+        pos1 -> X[i] = pos1 -> X[i + 1];
+        pos1 -> Y[i] = pos1 -> Y[i + 1];
+    }
+}
+
+
+void move_head(snake *snake1, snake_pos *pos1)
+{
+    switch (snake1 -> direction)
+    {
+        case right:
+            if(snake1 -> prev_direction == left)
+            {
+                snake1 -> head_X--;
+                break;
+            }
+                snake1 -> head_X++;
+                break;
+
+        case left:
+            if(snake1 -> prev_direction == right)
+            {
+                snake1 -> head_X++;
+                break;
+            }
+                snake1 -> head_X--;
+                break;
+
+        case up:
+            if(snake1 -> prev_direction == down)
+            {
+                snake1 -> head_Y++;
+                break;
+            }
+                snake1 -> head_Y--;
+                break;
+
+        case down:
+            if(snake -> prev_direction == up)
+            {
+                snake1 -> head_Y--;
+                break;
+            }
+                snake1 -> head_Y++;
+                break;
+
+        default:
+            break;
+    }
+
+    //update tail position here
+    pos1 -> X[snake1 -> size] = snake1 -> head_X;
+    pos1 -> Y[snake1 -> size] = snake1 -> head_Y;
+    gotoxy(pos1 -> X[snake1 -> size], pos1 -> Y[snake1 -> size]);
+    printf("%c", snake1 -> symbol);
+}
+
+//This is where food is initialized
+void food_init(food *food1)
+{
+    food1 -> X = (rand() % (horizontal - 5)) + 1;
+    food1 -> Y = (rand() % (vertical - 5)) + 1;
+    food1 -> symbol = 'F';
+}
+
+
+void food_print(food *food1)
+{
+    gotoxy(food1 -> X, food1 -> Y);
+    printf("%c", food1 -> symbol);
+}
+
+
+void gotoxy(int x, int y)
+{
+    printf("%c[%d;%df", 0x1B, x, y);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
